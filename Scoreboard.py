e104ebefc27bfe53e7d4124c09ee466ce8ffebc2
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-10-10 07:02:03 -0400

    Create Scoreboard.py
    display and update a scoreboard for the snake game

---
+++ Scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+ALIGNMENT = 'center'
+FONT = ('Courier', 15, 'normal')
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.speed('fastest')
+        self.color('white')
+        self.goto(0, 270)
+        self.points = 0
+        self.display_score()
+
+    def display_score(self):
+        self.write(f'Score: {self.points}', align=ALIGNMENT, font=FONT)
+
+    def keep_score(self):
+        self.points += 1
+        self.clear()
+        self.display_score()
