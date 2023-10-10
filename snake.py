2af3ebd5dfc8585fe9382dfabd6e6e6e06d21c5a
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-10-10 07:23:30 -0400

    Update snake.py
    added an extension condition

--- snake.py
+++ snake.py
@@ -1,6 +1,5 @@
 from turtle import Turtle
 
-
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -18,13 +17,18 @@ class Snake:
         self.head = self.segments[0]
 
     def create_snake(self):
-
         for position in STARTING_POSITIONS:
-            new_segment = Turtle('square')
-            new_segment.penup()
-            new_segment.color('white')
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, poosition):
+        new_segment = Turtle('square')
+        new_segment.penup()
+        new_segment.color('white')
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
 
@@ -46,6 +50,10 @@ class Snake:
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
     def right(self):
         if self.head.heading() != LEFT:
